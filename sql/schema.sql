af0e1f30b02a56a45e4a9c3e0469eec649a4f772
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-10-19 23:07:26 +0700

    feat : Fix init DB

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,72 +1,63 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS linkedin;
 
+-- Use the database
+USE linkedin;
 
-CREATE TABLE `user`
+-- Create user table
+CREATE TABLE IF NOT EXISTS `user`
 (
-    `user_id`  varchar(255) NOT NULL,
-    `email`    varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `role`     enum('jobseeker', 'company') NOT NULL,
-    `nama`     varchar(255) NOT NULL,
-    PRIMARY KEY (`user_id`)
+    `user_id`  VARCHAR(255) PRIMARY KEY,
+    `email`    VARCHAR(255)                  NOT NULL UNIQUE,
+    `password` VARCHAR(255)                  NOT NULL,
+    `role`     ENUM ('jobseeker', 'company') NOT NULL,
+    `nama`     VARCHAR(255)                  NOT NULL
 );
 
-CREATE TABLE `company_detail`
+-- Create company_detail table
+CREATE TABLE IF NOT EXISTS `company_detail`
 (
-    `user_id` varchar(255) NOT NULL,
-    `lokasi`  varchar(255) NOT NULL,
-    `about`   text         NOT NULL,
-    constraint fk_company_detail_user foreign key (user_id) references user (user_id)
+    `user_id` VARCHAR(255) PRIMARY KEY,
+    `lokasi`  VARCHAR(255) NOT NULL,
+    `about`   TEXT         NOT NULL,
+    CONSTRAINT fk_company_detail_user FOREIGN KEY (user_id) REFERENCES `user` (user_id)
 );
 
-CREATE TABLE `lowongan`
+-- Create lowongan table
+CREATE TABLE IF NOT EXISTS `lowongan`
 (
-    `lowongan_id`     varchar(255) NOT NULL,
-    `company_id`      varchar(255) NOT NULL,
-    `posisi`          varchar(255) NOT NULL,
-    `deskripsi`       text         NOT NULL,
-    `jenis_pekerjaan` varchar(255) NOT NULL,
-    `jenis_lokasi`    enum('on-site', 'hybrid', 'remote') NOT NULL,
-    `is_open`         tinyint(1) NOT NULL,
-    `created_at`      timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`      timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    constraint fk_lowongan_company foreign key (company_id) references user (user_id)
+    `lowongan_id`     VARCHAR(255) PRIMARY KEY,
+    `company_id`      VARCHAR(255)                         NOT NULL,
+    `posisi`          VARCHAR(255)                         NOT NULL,
+    `deskripsi`       TEXT                                 NOT NULL,
+    `jenis_pekerjaan` VARCHAR(255)                         NOT NULL,
+    `jenis_lokasi`    ENUM ('on-site', 'hybrid', 'remote') NOT NULL,
+    `is_open`         BOOLEAN                              NOT NULL,
+    `created_at`      TIMESTAMP                            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`      TIMESTAMP                            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_lowongan_company FOREIGN KEY (company_id) REFERENCES `user` (user_id)
 );
 
-CREATE TABLE `attachment_lowongan`
+-- Create attachment_lowongan table
+CREATE TABLE IF NOT EXISTS `attachment_lowongan`
 (
-    `attachment_id` int          NOT NULL AUTO_INCREMENT,
-    `lowongan_id`   varchar(255) NOT NULL,
-    `file_path`     varchar(255) NOT NULL,
-    constraint fk_attachment_lowongan_lowongan foreign key (lowongan_id) references lowongan (lowongan_id)
+    `attachment_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `lowongan_id`   VARCHAR(255) NOT NULL,
+    `file_path`     VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_attachment_lowongan_lowongan FOREIGN KEY (lowongan_id) REFERENCES `lowongan` (lowongan_id)
 );
 
-CREATE TABLE `lamaran`
+-- Create lamaran table
+CREATE TABLE IF NOT EXISTS `lamaran`
 (
-    `lamaran_id`    int          NOT NULL,
-    `user_id`       int          NOT NULL,
-    `lowongan_id`   int          NOT NULL,
-    `cv_path`       varchar(255) NOT NULL,
-    `video_path`    varchar(255) NOT NULL,
-    `status`        enum('accepted', 'rejected', 'waiting') NOT NULL,
-    `status_reason` text         NOT NULL,
-    `created_at`    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    constraint fk_lamaran_user foreign key (user_id) references user (user_id),
-    constraint fk_lamaran_lowongan foreign key (lowongan_id) references lowongan (lowongan_id)
-)
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+    `lamaran_id`    INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id`       VARCHAR(255)                             NOT NULL,
+    `lowongan_id`   VARCHAR(255)                             NOT NULL,
+    `cv_path`       VARCHAR(255)                             NOT NULL,
+    `video_path`    VARCHAR(255)                             NOT NULL,
+    `status`        ENUM ('accepted', 'rejected', 'waiting') NOT NULL,
+    `status_reason` TEXT                                     NOT NULL,
+    `created_at`    TIMESTAMP                                NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_lamaran_user FOREIGN KEY (user_id) REFERENCES `user` (user_id),
+    CONSTRAINT fk_lamaran_lowongan FOREIGN KEY (lowongan_id) REFERENCES `lowongan` (lowongan_id)
+);
